445cb3a4980942d1610de5836e753958fdc5b259
MostakemHossain <mostakememon123@gmail.com>
2024-02-18 09:11:45 +0600

    mobile nav added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from '@/components/Shared/MobileNav'
 import SideBar from '@/components/Shared/SideBar'
 import React from 'react'
 
@@ -5,6 +6,8 @@ const Layout = ({children}:{children:React.ReactNode}) => {
   return (
     <main className='root'>
       <SideBar/>
+      <MobileNav/>
+      
         <div className='root-container'>
             <div className='wrapper'>
             {children}
